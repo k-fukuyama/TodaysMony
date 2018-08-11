97ce2fb319ad114c8243d50f44541fe1e922f7ec
k-fukuyama <keitafukuyama0625@gmail.com>
2018-08-11 14:59:13 +0900

    tabbarを追加

---
+++ TodayMony/TABB.swift
@@ -0,0 +1,21 @@
+//
+//  TABB.swift
+//  TodayMony
+//
+//  Created by Keita Fukuyama on 2018/08/11.
+//  Copyright © 2018年 Keita Fukuyama. All rights reserved.
+//
+
+import UIKit
+
+class TABB: UITabBar {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
